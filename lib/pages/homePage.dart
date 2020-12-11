feab80163a530024295d2b12569c79d5065b8226
Aristocrat <crataristo4@gmail.com>
2020-12-11 12:36:23 +0000

    extracted FAB and body

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -31,71 +31,80 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: header(context, isAppTitle: true),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(context, MaterialPageRoute(builder: (context) {
-            return CreateOrEditNote();
-          }));
-        },
-        hoverColor: Colors.green,
-        backgroundColor: Colors.amber,
-        child: Icon(
-          Icons.add,
-          color: Colors.white,
-        ),
-        tooltip: AppConstants().addNote,
+        appBar: header(context, isAppTitle: true),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        floatingActionButton: _fab(context),
+        body: _body());
+  }
+
+  //floating action
+  _fab(context) {
+    return FloatingActionButton(
+      onPressed: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) {
+          return CreateOrEditNote();
+        }));
+      },
+      hoverColor: Colors.green,
+      backgroundColor: Colors.amber,
+      child: Icon(
+        Icons.add,
+        color: Colors.white,
       ),
-      body: ListView.separated(
-          itemBuilder: (context, index) {
-            return Dismissible(
-              background: Container(
-                color: Colors.red,
-                child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: Padding(
-                      padding: EdgeInsets.all(AppConstants().margin8),
-                      child: Icon(
-                        Icons.delete,
-                        color: Colors.white,
-                      ),
-                    )),
-              ),
-              direction: DismissDirection.startToEnd,
-              onDismissed: (onDismissed) {},
-              confirmDismiss: (confirmDismiss) async {
-                final results = await showDialog(
-                    context: context,
-                    builder: (_) {
-                      return SwipeToDeleteNote();
-                    });
-                print(results);
-                return results;
-              },
-              key: ValueKey(noteList[index].noteId),
-              child: ListTile(
-                onTap: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return CreateOrEditNote(
-                      noteId: noteList[index].noteId,
-                    );
-                  }));
-                },
-                title: Text(
-                  noteList[index].noteTitle,
-                  style: TextStyle(fontWeight: FontWeight.w700),
-                ),
-                subtitle: Text(
-                  'Last time edited : ${dateTimeFormatter(noteList[index].dateCreated)}',
-                  style: TextStyle(
-                      fontSize: AppConstants().font16, color: Colors.blue),
-                ),
-              ),
-            );
-          },
-          separatorBuilder: (context, index) => Divider(),
-          itemCount: noteList.length),
+      tooltip: AppConstants().addNote,
     );
+  }
+
+  //body
+  _body() {
+    return ListView.separated(
+        itemBuilder: (context, index) {
+          return Dismissible(
+            background: Container(
+              color: Colors.red,
+              child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Padding(
+                    padding: EdgeInsets.all(AppConstants().margin8),
+                    child: Icon(
+                      Icons.delete,
+                      color: Colors.white,
+                    ),
+                  )),
+            ),
+            direction: DismissDirection.startToEnd,
+            onDismissed: (onDismissed) {},
+            confirmDismiss: (confirmDismiss) async {
+              final results = await showDialog(
+                  context: context,
+                  builder: (_) {
+                    return SwipeToDeleteNote();
+                  });
+              print(results);
+              return results;
+            },
+            key: ValueKey(noteList[index].noteId),
+            child: ListTile(
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return CreateOrEditNote(
+                    noteId: noteList[index].noteId,
+                  );
+                }));
+              },
+              title: Text(
+                noteList[index].noteTitle,
+                style: TextStyle(fontWeight: FontWeight.w700),
+              ),
+              subtitle: Text(
+                'Last time edited : ${dateTimeFormatter(noteList[index].dateCreated)}',
+                style: TextStyle(
+                    fontSize: AppConstants().font16, color: Colors.blue),
+              ),
+            ),
+          );
+        },
+        separatorBuilder: (context, index) => Divider(),
+        itemCount: noteList.length);
   }
 }
